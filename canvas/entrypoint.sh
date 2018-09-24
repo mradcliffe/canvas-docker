9f15a781ca1be775d7137f679799dd33e6bb6d26
Matthew Radcliffe <mradcliffe@softpixel.com>
2018-09-24 10:45:37 -0400

    Initial commit of things that are working

---
+++ canvas/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Check to see if swapfile exists.
+if [ ! -d /swapfile ]; then
+  fallocate -l 4G /swapfile && \
+    mkswap /swapfile && \
+    swapon /swapfile
+fi
+
+RAILS_ENV=development \
+CANVAS_BUILD_CONCURRENCY=2 \
+USE_OPTIMIZED_JS=0 \
+DISABLE_HAPPYPACK=1 \
+COMPILE_ASSETS_STYLEGUIDE=0 \
+COMPILE_ASSETS_API_DOCS=0 \
+NODE_ENV=development \
+  ${RBENV_ROOT}/shims/bundle exec rake canvas:compile_assets && \
+  #RAILS_ENV=development ${RBENV_ROOT}/shims/bundle exec rake brand_configs:generate_and_upload_all && \
+  #RAILS_ENV=development ${RBENV_ROOT}/shims/bundle exec rake db:initial_setup && \
+  #${RBENV_ROOT}/shims/bundle exec rake assets:clobber && \
+  #${RBENV_ROOT}/shims/bundle exec rake assets:precompile && \
+  #${RBENV_ROOT}/shims/bundle exec rake db:migrate && \
+  /usr/local/rbenv/shims/passenger start -p 80 -e development --log-level 4
